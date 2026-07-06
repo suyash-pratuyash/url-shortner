5866b645f204ca42e15adb9c1f064fcf27b1a207
suyash-pratuyash <pratuyashsuyash@gmail.com>
2026-07-06 09:54:24 +0530

    add Url entity for URL mapping with JPA annotations

---
+++ src/main/java/com/suyash/url_shortner/entity/Url.java
@@ -0,0 +1,31 @@
+package com.suyash.url_shortner.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "url_mapping")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true , nullable = false)
+    private String shortcode;
+
+    @Column(nullable = false)
+    private String originalUrl;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
